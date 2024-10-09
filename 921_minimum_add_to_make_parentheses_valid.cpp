c2619b225e8cade5045691755ffe76e98596248b
Vyrx <58681073+Vyrx@users.noreply.github.com>
2024-10-09 11:48:51 +0800

    Add files via upload

---
+++ 921_minimum_add_to_make_parentheses_valid.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+
+        int open = 0, close = 0;
+        for (char c : s) {
+            if (c == '(') open++;
+            else {
+                if (open > 0) open--;
+                else close++;
+            }
+        }
+        return open + close;
+    }
+};
