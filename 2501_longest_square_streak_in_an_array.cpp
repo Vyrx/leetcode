406a62c5a10db9c5043a0c2ac1114aee9116099f
Vyrx <58681073+Vyrx@users.noreply.github.com>
2024-10-28 16:36:56 +0800

    Add files via upload

--- 2501_longest_square_streak_in_an_array.cpp
+++ 2501_longest_square_streak_in_an_array.cpp
@@ -1,38 +1,42 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<unordered_set>
 #include<unordered_map>
 using namespace std;
 
 class Solution {
 public:
 
-    int longestSquareStreak(vector<int>& nums) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
+	const int MAX_SQ = 46341;
+	int dp[100001]; // each possible value of nums[i]
 
-        sort(nums.begin(), nums.end());
-        
-        int cnt = 1;
-        unordered_map<long long, int> umap; // num, seq length
+	int longestSquareStreak(vector<int>& nums) {
+		ios_base::sync_with_stdio(0);
+		cin.tie(0);
+		cout.tie(0);
 
-        for (int i = nums.size() - 1; i >= 0; i--) {
-            if (nums[i] < 46341 && umap.count(nums[i] * nums[i])) {
-                umap[nums[i]] = umap[nums[i] * nums[i]] + 1;
-                cnt = max(cnt, umap[nums[i]]);
-            }
-            else
-                umap[nums[i]] = 1;
-            
-        }
+		memset(dp, 0, sizeof(dp));
+		int mx = 1;
+		for (auto it: nums) {
+			dp[it] = 1;
+			mx = max(mx, it);
+		}
 
-        return cnt == 1 ? -1 : cnt;
-    }
+		int cnt = 1;
+
+		for (int i = 2; i <= mx; i++) {
+			if (dp[i] && int(sqrt(i)) * int(sqrt(i)) == i) { // if i exists and is a perfect square
+				dp[i] += dp[int(sqrt(i))];
+				cnt = max(dp[i], cnt);
+			}
+		}
+		return cnt == 1 ? -1 : cnt;
+	}
 };
 
 void main() {
-    Solution S;
-    vector<int> nums({ 2, 3, 5, 6, 7 });
-    S.longestSquareStreak(nums);
+	Solution S;
+	vector<int> nums({ 4, 16, 2 });
+	cout << S.longestSquareStreak(nums);
 }
