0474c3ffa8871b120d401aaef8124be68bf3458d
Vyrx <58681073+Vyrx@users.noreply.github.com>
2024-11-02 17:46:36 +0800

    Add files via upload

---
+++ 2490_circular_sentence.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    bool isCircularSentence(string sentence) {
+        if (sentence.front() != sentence.back()) return false;
+
+        for (int i = 1; i < sentence.size() - 1; i++) {
+            if (sentence[i] == ' ' && sentence[i - 1] != sentence[i + 1]) return false;
+        }
+        return true;
+    }
+};
