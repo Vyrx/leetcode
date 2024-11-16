3263440d860b1ee02605466b60ffd824a97d3a4b
Vyrx <58681073+Vyrx@users.noreply.github.com>
2024-11-16 17:41:25 +0800

    Add files via upload

---
+++ 3254_find_the_power_of_k_size_subarrays_i.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> resultsArray(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+
+
+    }
+};
