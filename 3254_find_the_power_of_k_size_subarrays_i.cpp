a9dba6eeea18ed58ae5c20170f12cccb2a945746
Vyrx <58681073+Vyrx@users.noreply.github.com>
2024-11-16 17:41:40 +0800

    Add files via upload

--- 3254_find_the_power_of_k_size_subarrays_i.cpp
+++ 3254_find_the_power_of_k_size_subarrays_i.cpp
@@ -11,6 +11,33 @@ public:
         cin.tie(0);
         cout.tie(0);
 
+        if (k == 1) return nums;
 
+        int cur_len = 1;
+        vector<int> res;
+        for (int i = 1; i < k - 1; i++) {
+            if (nums[i] == nums[i - 1] + 1)
+                cur_len++;
+            else
+                cur_len = 1;
+        }
+
+        for (int i = k - 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1] + 1) {
+                cur_len++;
+                if (cur_len >= k) {
+                    cur_len = k - 1;
+                    res.push_back(nums[i]);
+                }
+                else 
+                    res.push_back(-1);
+            }
+            else {
+                cur_len = 1;
+                res.push_back(-1);
+            }
+        }
+
+        return res;
     }
 };
